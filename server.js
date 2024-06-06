cb281e025768ed6adc1a02025e3c8ad3a89e95b7
saravanan <saravanan.k1357@gmail.com>
2024-06-06 20:03:27 +0530

    fix bug while hosting

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const dotenv=require('dotenv')
 dotenv.config()
 // Mongoose connection
-mongoose.connect("mongodb+srv://saravanank24680:5W4Govdp6I3oUfmC@arcanesteps.x2cvb46.mongodb.net/SKS?retryWrites=true&w=majority&appName=arcanesteps")
+mongoose.connect(process.env.MONGO_ENV)
   .then(() => {
-    console.log("MongoDB is connected");
+    console.log("MongoDB is connected success");
   })
   .catch((err) => {
     console.log('MongoDB connection error:', err);
